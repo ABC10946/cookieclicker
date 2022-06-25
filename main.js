847ef94bcdaa2b2bfb010aa7f93d9757ab127c1f
ABC10946 <abctech10946@gmail.com>
2022-06-25 16:51:19 +0900

    ADD: save and load cookie count data

--- main.js
+++ main.js
@@ -3,12 +3,27 @@ cookieCount = 0
 window.onload = function() {
     cookie = document.getElementById('cookie')
     cookie.addEventListener('click', cookieClick)
-
+    loadData()
+    if (isNaN(cookieCount)) {
+        cookieCount = 0
+    }
 }
 
 function cookieClick() {
     console.log('cookie clicked!')
-    cookieCount++
+    cookieCount = cookieCount + 1
     cookieCounter = document.getElementById('cookie_counter')
     cookieCounter.innerText = cookieCount
+    saveData()
+}
+
+function saveData() {
+    localStorage.setItem('cookie', String(cookieCount))
+    console.log('save cookie count is:' + String(cookieCount))
+}
+
+function loadData() {
+    cookieCountStr = localStorage.getItem('cookie')
+    cookieCount = parseInt(cookieCountStr)
+    console.log('restore cookie count is:' + String(cookieCount))
 }
