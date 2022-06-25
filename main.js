86608b041ea701e56cf30dab8e5ac1d99977cc74
ABC10946 <abctech10946@gmail.com>
2022-06-25 16:26:54 +0900

    ADD: increment cookie count

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
+cookieCount = 0
+
 window.onload = function() {
     cookie = document.getElementById('cookie')
-    
+    cookie.addEventListener('click', cookieClick)
+
+}
+
+function cookieClick() {
+    console.log('cookie clicked!')
+    cookieCount++
+    cookieCounter = document.getElementById('cookie_counter')
+    cookieCounter.innerText = cookieCount
 }
